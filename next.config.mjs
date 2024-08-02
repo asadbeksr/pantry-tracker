3a87cd65cfbfe457d7d95b40a9f6953a2fd27446
asadbek <asadbeksr@gmail.com>
2024-08-02 05:16:18 +0500

    add env example

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        FIREBASE_API_KEY: 'AIzaSyBeaC-LfYcuQSi-TWP2vAtBveK1r3jD4tU',
-        FIREBASE_AUTH_DOMAIN: 'headstarterai.firebaseapp.com',
-        FIREBASE_PROJECT_ID: 'headstarterai',
-        FIREBASE_STORAGE_BUCKET: 'headstarterai.appspot.com',
-        FIREBASE_MESSAGING_SENDER_ID: '826023238898',
-        FIREBASE_APPID: '1:826023238898:web:469e527c79d87112ed046b',
-        FIREBASE_MEASUREMENT_ID: '',
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      FIREBASE_APPID: process.env.FIREBASE_APP_ID,
+      FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
     }
-};
-
-export default nextConfig;
+  };
+  
+  export default nextConfig;
